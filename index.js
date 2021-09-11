c6b685935f15559d0a3358a13742bcca04fc2d2c
colco09 <88566329+colco09@users.noreply.github.com>
2021-09-11 15:45:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,10 @@
 import express from "express";
 import cors from "cors";
-import mongoose from 'mongoose';
-
-import sample from './routes/sample.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-mongoose
-    .connect("mongodb+srv://annu_2020:annu_2020@cluster0.duywm.mongodb.net/cun?retryWrites=true&w=majority", {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        // useCreateIndex: true,
-        // useFindAndModify: true,
-    })
-    .then(() => console.log("Connected to database"))
-    .catch((err) => console.log(err));
-
-app.use("/new", sample);
 
 const PORT = process.env.PORT || 5000;
 
