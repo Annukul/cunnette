5c460e37615a52a024227b605fc36f37ca43110c
unknown <thakran9.anukul@gmail.com>
2021-09-11 15:16:25 +0530

    testing

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 import express from "express";
 import cors from "cors";
+import mongoose from 'mongoose';
+
+import sample from './routes/sample.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/new", sample);
+
+mongoose
+    .connect("mongodb://localhost:27017/cun", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        // useCreateIndex: true,
+        // useFindAndModify: true,
+    })
+    .then(() => console.log("Connected to database"))
+    .catch((err) => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
