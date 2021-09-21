4259ea4a888e7069d0eaae5a95aac3469c18ef36
Prashant <prashanttechjha@gmail.com>
2021-09-21 14:11:03 +0530

    User Detail & User Achievement

--- routes/userDetailsRoutes.js
+++ routes/userDetailsRoutes.js
@@ -1,9 +1,40 @@
 import express from "express";
 
-import { addDetails } from "../controllers/userDetailsControllers.js";
+import {
+  addUserDetails,
+  getUserDetail,
+  updateUserDetail,
+  addUserAchievements,
+  addUserSingleAchievement,
+  getUserAchievement,
+  getUserSingleAchievement,
+  updateUserAchievement,
+  deleteUserAchievement,
+} from "../controllers/userDetailsControllers.js";
 
 const router = express.Router();
 
-router.post("/addDetails", addDetails);
+router.post("/addUserDetails", addUserDetails);
+router.get("/getUserDetail/:id", getUserDetail);
+router.put("/updateUserDetail/:id", updateUserDetail);
+// User Achievements Routes
+router.post("/addUserAchievements", addUserAchievements);
+router.put(
+  "/addUserSingleAchievement/:id/:achievement",
+  addUserSingleAchievement
+);
+router.get("/getUserAchievements/:id", getUserAchievement);
+router.get(
+  "/getUserSingleAchievements/:id/:achievement/:arrayId",
+  getUserSingleAchievement
+);
+router.put(
+  "/updateUserAchievement/:id/:achievement/:index",
+  updateUserAchievement
+);
+router.delete(
+  "/updateUserAchievement/:id/:achievement/:arrayId",
+  deleteUserAchievement
+);
 
 export default router;
